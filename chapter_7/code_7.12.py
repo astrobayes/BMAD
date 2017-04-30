e5c2c87bcbd3386e8353ebc957a19c6b8015d3a4
Emille Ishida <emilleishida@gmail.com>
2017-05-01 00:09:38 +0200

    corrected 7.12

--- chapter_7/code_7.12.py
+++ chapter_7/code_7.12.py
@@ -4,48 +4,48 @@
 # you are kindly asked to include the complete citation if you used this 
 # material in a publication
 
-# Code 7.12 - Bayesian lognormal-logit hurdle model in Python using Stan
-# 1 response (y) and 2 explanatory variables (x1, x2)
-#
+# Code 7.12 - Bayesian log-gamma–logit hurdle model in Python using Stan
+
 
 import numpy as np
-import pystan 
+import pystan
 import statsmodels.api as sm
-
-from scipy.stats import uniform, bernoulli
+from scipy.stats import uniform, gamma, bernoulli
 
 # Data
-np.random.seed(33559)                                 # set seed to replicate example
-nobs= 2000                                            # number of obs in model 
+np.random.seed(33559)                             # set seed to replicate example
+nobs = 1000                                       # number of obs in model
 
-x1 = uniform.rvs(loc=0, scale=2.5, size=nobs)
-xc = 0.6 + 1.25 * x1                                  # linear predictor, xb
-y = np.random.lognormal(sigma=0.4, mean=np.exp(xc))
+# Generate predictors, design matrix
+x1 = uniform.rvs(loc=0, scale=4, size=nobs)
+xc = -1 + 0.75 * x1
+exc = np.exp(xc)
+phi = 0.066
+r = 1.0/phi
+y = np.random.gamma(shape=exc, scale=r)
 
-xb = -3.0 + 4.5 * x1                                  # construct filter
-pi = 1.0/(1.0 + np.exp(-xb))
-bern = [bernoulli.rvs(1-pi[i]) for i in range(nobs)]
-
-ly = [y[i]*bern[i] for i in  range(nobs)]             # Add structural zeros
-
-
+# Construct filter
+xb = -2 + 1.5 * x1
+pi = 1 / (1 + np.exp(-xb))
+bern = bernoulli.rvs(1-pi)
+gy = [y[i]*bern[i] for i in range(nobs)]          # add structural zeros
 X = np.transpose(x1)
 X = sm.add_constant(X)
 
-mydata = {}                                # build data dictionary
-mydata['Y'] = ly                           # response variable
-mydata['N'] = nobs                         # sample size
-mydata['Xb'] = X                           # predictors         
-mydata['Xc'] = X
-mydata['Kb'] = X.shape[1]                  # number of coefficients
-mydata['Kc'] = X.shape[1] 
-
 # Fit
+mydata = {}                                       # build data dictionary
+mydata['Y'] = gy                                  # response variable
+mydata['N'] = nobs                                # sample size
+mydata['Xb'] = X                                  # predictors
+mydata['Xc'] = X
+mydata['Kb'] = X.shape[1]                         # number of coefficients
+mydata['Kc'] = X.shape[1]
+
 stan_code = """
 data{
-    int<lower=0> N;
-    int<lower=0> Kb;
-    int<lower=0> Kc;
+    int N;
+    int Kb;
+    int Kc;
     matrix[N, Kb] Xb;
     matrix[N, Kc] Xc;
     real<lower=0> Y[N];
@@ -53,29 +53,25 @@ data{
 parameters{
     vector[Kc] beta;
     vector[Kb] gamma;
-    real<lower=0> sigmaLN;
+    real<lower=0> phi;
 }
 model{
     vector[N] mu;
     vector[N] Pi;
-
     mu = exp(Xc * beta);
+
     for (i in 1:N) Pi[i] = inv_logit(Xb[i] * gamma);
 
     for (i in 1:N) {
         (Y[i] == 0) ~ bernoulli(Pi[i]);
-        if (Y[i] > 0) Y[i] ~ lognormal(mu[i], sigmaLN);
+        if (Y[i] > 0) Y[i] ~ gamma(mu[i], phi) T[0,];
     }
 }
 """
 
-
 # Run mcmc
-fit = pystan.stan(model_code=stan_code, data=mydata, iter=7000, chains=3,
+fit = pystan.stan(model_code=stan_code, data=mydata, iter=6000, chains=3,
                   warmup=4000, n_jobs=3)
 
 # Output
-print(fit)  
-
-
-
+print (fit)
