595156d1e4ebd815928674382a4c92145b09873b
Emille Ishida <emilleishida@gmail.com>
2017-05-01 16:20:00 +0200

    renamed file

--- chapter_8/code_8.7.py
+++ chapter_8/code_8.7.py
@@ -4,99 +4,52 @@
 # you are kindly asked to include the complete citation if you used this 
 # material in a publication
 
-# Data from Code 8.5b - Simulated random intercept binary logistic data
+# Code 8.7 - Bayesian random intercept binary logistic model in Python using pymc3
 
 import numpy as np
-import statsmodels.api as sm
+import pymc3 as pm
 
 from scipy.stats import norm, uniform, bernoulli
 
 # Data
 np.random.seed(13531)                 # set seed to replicate example
-N = 40000                              # number of obs in model 
+N = 4000                              # number of obs in model 
 NGroups = 20
 
 x1 = uniform.rvs(size=N)
 x2 = uniform.rvs(size=N)
-Groups = np.array([2000 * [i] for i in range(20)]).flatten()
+Groups = np.array([200 * [i] for i in range(20)]).flatten()
 
-#a = norm.rvs(loc=0, scale=0.5, size=NGroups)
-a = np.array([0.579, -0.115, -0.125, 0.169, -0.500, -1.429, -1.171, -0.205, 0.193,
-0.041, -0.917, -0.353, -1.197, 1.044, 1.084, -0.085, -0.886, -0.352,
--1.398, 0.350])
+a = norm.rvs(loc=0, scale=0.5, size=NGroups)
 
 eta = 1 + 0.2 * x1 - 0.75 * x2 + a[list(Groups)]
 mu = 1.0/(1.0 + np.exp(-eta))
-y = bernoulli.rvs(mu)
+y = bernoulli.rvs(mu, size=N)
 
-X = sm.add_constant(np.column_stack((x1,x2)))
-K = X.shape[1]
-re = Groups
-Nre = NGroups
+with pm.Model() as model: 
+    # Define priors
+    sigma = pm.Uniform('sigma', 0, 100)
+    sigma_a = pm.Uniform('sigma_a', 0, 10)
+    beta1 = pm.Normal('beta1', 0, sd=100)
+    beta2 = pm.Normal('beta2', 0, sd=100)
+    beta3 = pm.Normal('beta3', 0, sd=100)
+    
+    # priors for random intercept (RI) parameters
+    a_param = pm.Normal('a_param',
+                         np.repeat(0, NGroups),                   # mean
+                         sd=np.repeat(sigma_a, NGroups),          # standard deviation
+                         shape=NGroups)                           # number of RI parameters
 
+    eta = beta1 + beta2*x1 + beta3*x2 + a_param[Groups]
+    
+    # Define likelihood
+    y = pm.Normal('y', mu=1.0/(1.0 + pm.exp(-eta)), sd=sigma, observed=y)
+    
+    # Fit
+    start = pm.find_MAP()                        # Find starting value by optimization
+    step = pm.NUTS(state=start)                  # Initiate sampling 
+    trace = pm.sample(7000, step, start=start)     
 
-model_data = {}
-model_data['Y'] = y
-model_data['X'] = X                              
-model_data['K'] = K
-model_data['N'] = N
-model_data['NGroups'] = NGroups
-model_data['re'] = re
-model_data['b0'] = np.repeat(0, K) 
-model_data['B0'] = np.diag(np.repeat(100, K))
-model_data['a0'] = np.repeat(0, Nre)
-model_data['A0'] = np.diag(np.repeat(1, Nre))
-
-# Code 8.7b - Bayesian random intercept binary logistic model in Python using Stan
-
-import pystan
-
-# Fit
-stan_code = """
-data{
-    int<lower=0> N;
-    int<lower=0> K;
-    int<lower=0> NGroups;
-    matrix[N, K] X;
-    int<lower=0> Y[N];
-    int re[N];
-    vector[K] b0;
-    matrix[K, K] B0;
-    vector[NGroups] a0;
-    matrix[NGroups, NGroups] A0;
-}
-parameters{
-    vector[K] beta;
-    vector[NGroups] a;
-    real<lower=0, upper=100> sigma_re;
-}
-transformed parameters{
-    vector[N] eta;
-    vector[N] p; 
- 
-    eta = X * beta;
-    for (i in 1:N){ 
-        p[i] = 1.0/(1.0 + exp(-(eta[i] + a[re[i]+1])));
-    }
-}
-model{    
-    #sigma_re ~ cauchy(0, 25);
-
-    beta ~ multi_normal(b0, B0);
-    a ~ multi_normal(a0, sigma_re * A0);
-
-    Y ~ bernoulli(p);  
-}
-"""
-
-fit = pystan.stan(model_code=stan_code, data=model_data, iter=10000, chains=3, thin=10,
-                  warmup=6000, n_jobs=3)
-
-# Output
-nlines = 29                                  # number of lines in screen output
-
-output = str(fit).split('\n')
-for item in output[:nlines]:
-    print(item)  
-
+# Print summary to screen
+pm.summary(trace)
 
