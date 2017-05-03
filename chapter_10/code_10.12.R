fa97fb7bcadfd89a55ac7491208efe84f71fcee6
Emille Ishida <emilleishida@gmail.com>
2017-05-03 18:55:58 +0200

    fix format

--- chapter_10/code_10.12.R
+++ chapter_10/code_10.12.R
@@ -9,41 +9,51 @@
 
 require(R2jags)
 # Data
-path_to_data = '../data/Section_10p6/Red_spirals.csv'
+path_to_data = 'https://raw.githubusercontent.com/astrobayes/BMAD/master/data/Section_10p6/Red_spirals.csv'
+
 # Read data
 Red <- read.csv(path_to_data,header=T)
+
 # Prepare data to JAGS
 N <- nrow(Red)
 x <- Red$fracdeV
 y <- Red$type
+
 # Construct data dictionary
 X <- model.matrix(~ x,
                    data = Red)
+
 K <- ncol(X)
+
 logit_data <- list(Y = y, # response variable
                    X = X, # predictors
                    N = N, # sample size
                    K = K # number of columns
 )
+
 # Fit
 LOGIT <-"model{
-# Diffuse normal priors
-for(i in 1:K){
-beta[i] ~ dnorm(0, 1e-4)
-}
-# Likelihood function
-for (i in 1:N){
-Y[i] ~ dbern(p[i])
-logit(p[i]) <- eta[i]
-eta[i] <- inprod(beta[], X[i,])
-}
+    # Diffuse normal priors
+    for(i in 1:K){
+        beta[i] ~ dnorm(0, 1e-4)
+    }
+
+    # Likelihood function
+    for (i in 1:N){
+        Y[i] ~ dbern(p[i])
+        logit(p[i]) <- eta[i]
+        eta[i] <- inprod(beta[], X[i,])
+    }
 }"
+
 # Define initial values
 inits <- function () {
   list(beta = rnorm(ncol(X), 0, 0.1))
 }
+
 # Identify parameters
 params <- c("beta")
+
 # Fit
 LOGIT_fit <- jags(data = logit_data,
                   inits = inits,
@@ -53,5 +63,6 @@ LOGIT_fit <- jags(data = logit_data,
                   n.chains = 3,
                   n.burnin = 3000,
                   n.iter = 6000)
+
 # Output
 print(LOGIT_fit,intervals=c(0.025, 0.975),justify = "left", digits=2)
