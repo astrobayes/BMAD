227e257543b2aa027698ccf39741b885b3894c81
Emille Ishida <emilleishida@gmail.com>
2017-05-03 20:20:52 +0200

    fix format

--- chapter_10/code_10.15.R
+++ chapter_10/code_10.15.R
@@ -9,49 +9,62 @@
 
 require(R2jags)
 require(jagstools)
+
 # Data
 path_to_data = "../data/Section_10p7/GCs.csv"
+
 # Read data
 GC_dat = read.csv(file=path_to_data,header = T,dec=".")
+
 # Prepare data to JAGS
 N <- nrow(GC_dat)
 x <- GC_dat$MV_T
 y <- GC_dat$N_GC
 X <- model.matrix(~ x, data=GC_dat)
 K = ncol(X)
+
 JAGS_data <- list(
   Y = y,
   X = X,
   N = N,
   K = K)
 
+
+
 # Code 10.15 - Negative binomial model, in R using JAGS, for modeling the relationship
 #              between globular cluster population and host galaxy visual magnitude
 
 # Fit
 model.NB <- "model{
-# Diffuse normal priors betas
-for (i in 1:K) { beta[i] ~ dnorm(0, 1e-5)}
-# Prior for theta
-theta ~ dgamma(1e-3,1e-3)
-for (i in 1:N){
-eta[i] <- inprod(beta[], X[i,])
-mu[i] <- exp(eta[i])
-p[i] <- theta/(theta+mu[i])
-Y[i] ~ dnegbin(p[i],theta)
-# Discrepancy
-expY[i] <- mu[i] # mean
-varY[i] <- mu[i] + pow(mu[i],2)/theta # variance
-PRes[i] <- ((Y[i] - expY[i])/sqrt(varY[i]))^2
-}
-Dispersion <- sum(PRes)/(N-3)
+    # Diffuse normal priors betas
+    for (i in 1:K) { beta[i] ~ dnorm(0, 1e-5)}
+
+    # Prior for theta
+    theta ~ dgamma(1e-3,1e-3)
+
+    for (i in 1:N){
+        eta[i] <- inprod(beta[], X[i,])
+        mu[i] <- exp(eta[i])
+        p[i] <- theta/(theta+mu[i])
+        Y[i] ~ dnegbin(p[i],theta)
+
+        # Discrepancy
+        expY[i] <- mu[i] # mean
+        varY[i] <- mu[i] + pow(mu[i],2)/theta # variance
+        PRes[i] <- ((Y[i] - expY[i])/sqrt(varY[i]))^2
+    } 
+
+    Dispersion <- sum(PRes)/(N-3)
 }"
+
 # Define initial values
 inits <- function () {
   list(beta = rnorm(K, 0, 0.1))
 }
+
 # Identify parameters
 params <- c("beta","theta","Dispersion")
+
 # Start JAGS
 NB_fit <- jags(data = JAGS_data ,
                inits = inits,
@@ -61,8 +74,10 @@ NB_fit <- jags(data = JAGS_data ,
                n.chains = 3,
                n.burnin = 3500,
                n.iter = 7000)
+
 # Output
 # Plot posteriors
 MyBUGSHist(out,c("Dispersion",uNames("beta",K),"theta"))
+
 # Dump results on screen
 print(NB_fit, intervals=c(0.025, 0.975), digits=3)
